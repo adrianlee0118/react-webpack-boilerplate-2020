cc0178d3a73aa598634f5f7ddd763cecb605eccf
Adrian Lee <adrianlee0118@gmail.com>
2020-07-03 11:18:14 -0700

    removed unnecessary files

--- src/archive/Table-Destructured/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env browser */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './archive/Table-Destructured/App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-module.hot.accept();
